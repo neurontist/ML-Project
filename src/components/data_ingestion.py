2e0388abe92a8841c47bf788f210818d798e0c45
neurontist <aliza.sayyed@outlook.com>
2025-10-29 01:52:55 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -43,12 +43,3 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys)
 
-if __name__ == '__main__':
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
-
-    data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.transformation(train_data, test_data)
-
-    modelTrainer = ModelTrainer()
-    print(modelTrainer.initiate_model_training(train_arr, test_arr))
